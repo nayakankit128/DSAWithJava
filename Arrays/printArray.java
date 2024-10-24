21e2071ee3ed2ee26b950a85ad433950656cfa25
Ankit Nayak <nayakankit128@gmail.com>
2024-10-24 20:24:25 +0530

    print the array and count the element of this array

--- Arrays/printArray.java
+++ Arrays/printArray.java
@@ -3,6 +3,14 @@ package Arrays;
 public class printArray {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+          
+        int n[] = {56,89,65,32,53,16,48,58,89};
+        int count = 0;
+        for(int i=0; i<n.length; i++){
+            System.out.print(n[i]+" ");
+            count++;
+        }
+        System.out.println();
+        System.out.println(count);
     }
 }
